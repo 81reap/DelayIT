e0317605be224effa55e6b9d41854bcdbc0d59d4
Hax0rDoge <p.bhakar.work@gmail.com>
2017-07-07 23:06:27 -0500

    now creates posts and sends notifications

--- DelayIT.py
+++ DelayIT.py
@@ -57,14 +57,21 @@ def sms(msg):
 # inf loop										------------------------
 k = 0
 while k < 1:
+	# load stored info
 	accounts = j.loads(open("accounts.json",'r').read())
 	posts = j.loads(open("posts.json",'r').read())
+	# get current time
 	now = d.now().strftime("%Y %m %d %H:%M")
 
+	# loop though all the posts
 	for i in range(len(posts)):
+		# parse preset post time
 		postTime = d.strptime(posts[i]["date"],"%Y %m %d %H:%M")
 		postTime = postTime.strftime("%Y %m %d %H:%M")
+
+		# check if the post should go up
 		if posts[i]["status"] == "waiting": #and postTime == now:
+			# initialize reddit account
 			reddit = r.Reddit(
 				client_id=accounts[posts[i]["account"]]["id"],
 				client_secret=accounts[posts[i]["account"]]["secret"],
@@ -72,11 +79,56 @@ while k < 1:
 				user_agent=USER_AGENT,
 				usernmae=accounts[posts[i]["account"]]["user"]
 				)
-			print(reddit)
-			if 0 == "success":
+			print(reddit.user.me())
+
+			# create a reddit post
+			submission = ""
+			if posts[i]["link"] != "":
+				submission = reddit.subreddit(posts[i]["subreddit"]).submit(
+					posts[i]["title"],
+					url=posts[i]["link"],
+					)
+			elif posts[i]["text"] != "":
+				submission = reddit.subreddit(posts[i]["subreddit"]).submit(
+					posts[i]["title"],
+					selftext=posts[i]["text"],
+					)
+
+			print(submission)
+			# if the submission type is an error
+			if type(submission) is PRAWException:
+				if type(submission) is ClientException:
+					sms("The client failed to post")
+				elif type(submission) is APIException:
+					sms("Reddit failed to post")
+				else:
+					sms("Something went wrong")
+			# if the submission was a success
+			else:
+				# adds a flair to the submission if specified
+				if posts[i]["flair"] != "":
+					choices = submission.flair.choices()
+					print(choices)
+					template_id = next(
+						x for x in choices 
+						if x['flair_text_editable']
+						)['flair_template_id']
+					print(template_id)
+					submission.flair.select(
+						template_id, 
+						'custom value'
+						)
+
+				# follow up with a attached comment if specified
+				if posts[i]["comment"] != "":
+					submission.reply(posts[i]["comment"])
+
+				# change the status in posts.json
 				posts[i]["status"] = "posted"
 				j.dump(posts, indent=4)
-	#sms("about to post")
+			
+				# notify the user about the post
+				sms("Your post, " + posts[i]["title"] + ", has been posted to /r/" + posts[i]["subreddit"] + " on " + now + ". Here's a link: " + submission.shortlink)
 
 	k = k+1
 	t.sleep(1)
